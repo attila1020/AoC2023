ba706d26ae4223eeae599a33743bb5f754d2d935
attila1020 <attila.miszkuly@gmail.com>
2023-12-28 18:02:40 +0100

    Part 2 answer

--- day01/src/Main.java
+++ day01/src/Main.java
@@ -6,13 +6,21 @@ public class Main {
 		List<String> calibrations = FileUtils.readLines("input.txt");
 
 		Part1 Part1 = new Part1();
+		Part2 Part2 = new Part2();
 
 		int part1Result = calibrations.stream()
 				.map(Part1::filterString)
 				.map(Part1::getValue)
 				.reduce(0, Integer::sum);
 
+		int part2Result = calibrations.stream()
+						.map(Part2::brigiFuggveny)
+						.map(Part1::filterString)
+						.map(Part1::getValue)
+						.reduce(0, Integer::sum);
+
 		System.out.println("Part 1 Result: " + part1Result);
+		System.out.println("Part 2 Result: " + part2Result);
 	}
 
 }
--- day01/src/Part2.java
+++ day01/src/Part2.java
@@ -1,3 +1,19 @@
+import java.util.HashMap;
+
 public class Part2 {
 
+    HashMap<String, String> map = new HashMap<>();
+    public String brigiFuggveny(String s) {
+
+        return s.replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five","five5five")
+                .replace("six","six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight","eight8eight")
+                .replace("nine","nine9nine");
+
+    }
 }
